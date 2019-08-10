e2384e69db90fd32b38715131382f3ac3ce9a4b9
Maryanne Heinbaugh <23heinbau@gmail.com>
2019-08-10 16:45:48 -0700

    updated seeds file

--- models/db/seeds.js
+++ models/db/seeds.js
@@ -5,69 +5,69 @@ var faker = require('faker');
 const db = require('../../models')
 
 //Business Table
-for (let i = 0; i < 30; i++) {
-  db.business.create({
-    BusinessName: faker.company.companyName(),
-    PayPeriod: faker.random.arrayElement(['weekly', 'biweekly', 'semi-monthly', 'monthly']),
-    PayrollSystem: faker.random.arrayElement(["Quickbook", "ADP","Patriot Software","Gusto", "Deluxe", "Square", "PAYCHEX", "Paycor", "zenefits", "Justworks"]),     
-  })
+// for (let i = 0; i < 30; i++) {
+//   db.business.create({
+//     BusinessName: faker.company.companyName(),
+//     PayPeriod: faker.random.arrayElement(['weekly', 'biweekly', 'semi-monthly', 'monthly']),
+//     PayrollSystem: faker.random.arrayElement(["Quickbook", "ADP","Patriot Software","Gusto", "Deluxe", "Square", "PAYCHEX", "Paycor", "zenefits", "Justworks"]),     
+//   })
   
   
-};
-// Employee Table
-for (let i = 0; i < 30; i++) {
-db.employee.create({
+// };
+// // Employee Table
+// for (let i = 0; i < 30; i++) {
+// db.employee.create({
      
-    FirstName: faker.name.firstName(),
-    LastName: faker.name.lastName(),
-    DOB: faker.date.between('1999-01-01', '2002-01-05'),
-    Startdate: faker.date.between('2018-01-01','2019-08-01'),
-    Email: faker.internet.email(),
-    Phone: faker.phone.phoneNumber(),
-    MinHours: faker.random.number({min: 2,max: 4}),
-    MaxHours: faker.random.number({min: 8,max: 20}),
-    Wage: faker.random.arrayElement([12.50, 13.00,13.25, 12.75, 12.00, 12.75]),
-    CertExpDate: faker.date.between('2018-01-01','2020-08-01'),
-    CertType: "license",
-    Password: faker.internet.password(),
-    businessBusinessID: 1,
+//     FirstName: faker.name.firstName(),
+//     LastName: faker.name.lastName(),
+//     DOB: faker.date.between('1999-01-01', '2002-01-05'),
+//     Startdate: faker.date.between('2018-01-01','2019-08-01'),
+//     Email: faker.internet.email(),
+//     Phone: faker.phone.phoneNumber(),
+//     MinHours: faker.random.number({min: 2,max: 4}),
+//     MaxHours: faker.random.number({min: 8,max: 20}),
+//     Wage: faker.random.arrayElement([12.50, 13.00,13.25, 12.75, 12.00, 12.75]),
+//     CertExpDate: faker.date.between('2018-01-01','2020-08-01'),
+//     CertType: "license",
+//     Password: faker.internet.password(),
+//     businessBusinessID: 1,
     
-  })
-};
+//   })
+// };
     
-// Address Table    
-for (let i = 0; i < 30; i++) {
-  db.address.create({   
-    Street: faker.address.streetAddress(),
-    City: faker.random.arrayElement(["Seattle", "Bellevue","Kirkland","Issaquah", "Redmond", "Bothell", "Lynnwood", "Everett", "Renton", "Woodinville", "North Bend"]),
-    State: "WA",
-    Zipcode: faker.random.arrayElement(["98040", "98011", "98072", "98077","98101", "98114", "98113", "98004"]),   
-  })
-};
+// // Address Table    
+// for (let i = 0; i < 30; i++) {
+//   db.address.create({   
+//     Street: faker.address.streetAddress(),
+//     City: faker.random.arrayElement(["Seattle", "Bellevue","Kirkland","Issaquah", "Redmond", "Bothell", "Lynnwood", "Everett", "Renton", "Woodinville", "North Bend"]),
+//     State: "WA",
+//     Zipcode: faker.random.arrayElement(["98040", "98011", "98072", "98077","98101", "98114", "98113", "98004"]),   
+//   })
+// };
 
 
-// Role Table
-let roles = ['Counter', 'Grill', 'Fry', 'Manager'];
-for (let i = 0; i < roles.length; i++) {
-  db.role.create({
-    RoleName: roles[i]
-  });
-};
+// // Role Table
+// let roles = ['Counter', 'Grill', 'Fry', 'Manager'];
+// for (let i = 0; i < roles.length; i++) {
+//   db.role.create({
+//     RoleName: roles[i]
+//   });
+// };
 
 
-// Employee_roles Table
-let employeeCount = 30;
+// // Employee_roles Table
+// let employeeCount = 30;
 
-for (let i = 1; i < employeeCount + 1; i++) {
-  //const randomRole = faker.random.number({min: 1, max: 3});
-  db.employee_roles.create({
+// for (let i = 1; i < employeeCount + 1; i++) {
+//   //const randomRole = faker.random.number({min: 1, max: 3});
+//   db.employee_roles.create({
         
-    ProficiencyLevel: faker.random.arrayElement(
-      ["inexperienced", "novice", "competent"]),   
-    EmployeeID: i,
-    RoleID: faker.random.number({min: 1,max: 3})
-  })
-}
+//     ProficiencyLevel: faker.random.arrayElement(
+//       ["inexperienced", "novice", "competent"]),   
+//     EmployeeID: i,
+//     RoleID: faker.random.number({min: 1,max: 3})
+//   })
+// }
 
 
 
@@ -77,6 +77,7 @@ for (let i=1; i<11; i++){
     switch (i){
       case 1:
         db.shift.create({
+          Date: "2019/8/"+(14+i),
           StartTime: 0830,
           EndTime: 1700,
           ClockInTime: 0830,
@@ -87,6 +88,7 @@ for (let i=1; i<11; i++){
 
       case 2:
           db.shift.create({
+            Date: "2019/8/"+(14+i),
             StartTime: 0900,
             EndTime: 1630,
             ClockInTime: 0900,
@@ -98,6 +100,7 @@ for (let i=1; i<11; i++){
 
       case 3:
           db.shift.create({
+            Date: "2019/8/"+(14+i),
             StartTime: 1000,
             EndTime: 1700,
             ClockInTime: 1000,
@@ -108,6 +111,7 @@ for (let i=1; i<11; i++){
           
       case 4:        
           db.shift.create({
+            Date: "2019/8/"+(14+i),
             StartTime: 1100,
             EndTime: 1400,
             ClockInTime: 1100,
@@ -118,6 +122,7 @@ for (let i=1; i<11; i++){
 
       case 5:
           db.shift.create({
+            Date: "2019/8/"+(14+i),
             StartTime: 1100,
             EndTime: 1730,
             ClockInTime: 1100,
@@ -128,6 +133,7 @@ for (let i=1; i<11; i++){
 
       case 6:
           db.shift.create({
+            Date: "2019/8/"+(14+i),
             StartTime: 1400,
             EndTime: 1700,
             ClockInTime: 1400,
@@ -138,6 +144,7 @@ for (let i=1; i<11; i++){
 
       case 7:
           db.shift.create({
+            Date: "2019/8/"+(14+i),
             StartTime: 1630,
             EndTime: 2030,
             ClockInTime: 1630,
@@ -148,6 +155,7 @@ for (let i=1; i<11; i++){
 
       case 8:
           db.shift.create({
+            Date: "2019/8/"+(14+i),
             StartTime: 1700,
             EndTime: 2100,
             ClockInTime: 1700,
@@ -158,6 +166,7 @@ for (let i=1; i<11; i++){
 
       case 9:
           db.shift.create({
+            Date: "2019/8/"+(14+i),
             StartTime: 1730,
             EndTime: 2130,
             ClockInTime: 1730,
@@ -168,6 +177,7 @@ for (let i=1; i<11; i++){
 
       case 10:
           db.shift.create({
+            Date: "2019/8/"+(14+i),
             StartTime: 1730,
             EndTime: 2200,
             ClockInTime: 1730,
